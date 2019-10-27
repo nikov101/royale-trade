2d1842b76399d25b038240776e6d244c671e8c69
nikov101 <crazyru04286@gmail.com>
2019-10-27 17:26:02 +0800

    :bug: Fix wrong text in ChooseCardModal

--- src/components/ChooseCardModal.js
+++ src/components/ChooseCardModal.js
@@ -1,15 +1,14 @@
 import './ChooseCards.css';
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, injectIntl } from 'react-intl';
 import { Container } from 'semantic-ui-react';
 import { Modal } from 'semantic-ui-react';
 
 import CardList from './CardList';
 import ChooseCards from './ChooseCards';
 
-export default class ChooseCardsModal extends React.Component{
-
+class ChooseCardsModal extends React.Component {
   state = {
     sendButtonToggle: true,
     step: 1,
@@ -21,7 +20,7 @@ export default class ChooseCardsModal extends React.Component{
     textAreaPlayerTag: '',
     emptyTimeError: false,
     emptyClanError: false,
-    emptyPlayerNameError: false,
+    emptyPlayerNameError: false
   };
 
   // toggle = () => {
@@ -41,7 +40,7 @@ export default class ChooseCardsModal extends React.Component{
   handleWant = index => {
     let choosed = this.state.wantCards.slice();
     choosed[index] = !choosed[index];
-    this.setState({ 
+    this.setState({
       wantCards: choosed
     });
   };
@@ -54,7 +53,7 @@ export default class ChooseCardsModal extends React.Component{
     });
   };
 
-  handleOnChangePlayerName = (event) => {
+  handleOnChangePlayerName = event => {
     if (event.target.value.length > 20) {
       console.log('textarea Name too long.');
       return;
@@ -63,17 +62,17 @@ export default class ChooseCardsModal extends React.Component{
       textAreaPlayerName: event.target.value,
       emptyPlayerNameError: event.target.value.length === 0
     });
-  }
+  };
 
-  handleOnChangePlayerTag = (event) => {
+  handleOnChangePlayerTag = event => {
     if (event.target.value.length > 15) {
       console.log('textarea Tag too long.');
       return;
     }
     this.setState({ textAreaPlayerTag: event.target.value });
-  }
+  };
 
-  handleOnChangeTime = (event) => {
+  handleOnChangeTime = event => {
     if (event.target.value.length > 100) {
       console.log('textarea Time too long.');
       return;
@@ -82,23 +81,25 @@ export default class ChooseCardsModal extends React.Component{
       textAreaTime: event.target.value,
       emptyTimeError: event.target.value.length === 0
     });
-  }
+  };
 
-  handleOnChangeClan = (event) => {
+  handleOnChangeClan = event => {
     if (event.target.value.length > 50) {
       console.log('textarea clan too long.');
       return;
     }
-    this.setState({ 
+    this.setState({
       textAreaClan: event.target.value,
       emptyClanError: event.target.value.length === 0
     });
-  }
+  };
 
   postTradeOffer = async () => {
-    if (this.state.textAreaTime.length === 0 
-        || this.state.textAreaClan.length === 0 
-        || this.state.textAreaPlayerName.length === 0) {
+    if (
+      this.state.textAreaTime.length === 0 ||
+      this.state.textAreaClan.length === 0 ||
+      this.state.textAreaPlayerName.length === 0
+    ) {
       this.setState({
         emptyTimeError: this.state.textAreaTime.length === 0,
         emptyClanError: this.state.textAreaClan.length === 0,
@@ -108,9 +109,9 @@ export default class ChooseCardsModal extends React.Component{
       return;
     }
     await this.props.postTradeOffer(
-      this.state.wantCards, 
-      this.state.offerCards, 
-      this.state.textAreaTime, 
+      this.state.wantCards,
+      this.state.offerCards,
+      this.state.textAreaTime,
       this.state.textAreaClan,
       this.state.textAreaPlayerName,
       this.state.textAreaPlayerTag
@@ -119,57 +120,64 @@ export default class ChooseCardsModal extends React.Component{
   };
 
   render() {
+    const { intl } = this.props;
     return (
       <div>
         <Modal open onClose={this.goHome} size="large">
-          <Modal.Header>{<FormattedMessage id='myTradeOffer'/>}</Modal.Header>
+          <Modal.Header>
+            <FormattedMessage id="myTradeOffer" />
+          </Modal.Header>
           <Modal.Content>
             <Container>
               {/* <Row>
                 <Col> */}
-                  <ChooseCards
-                    choosable={this.state.step === 1}
-                    header={<FormattedMessage id='wantCard'/>}
-                    title={<FormattedMessage id='selectAll'/>}
-                    choosed={this.state.wantCards}
-                    onToggle={this.handleWant}
-                    onClickNext={() => this.setState({ step: 2 })}
-                    onClickExpand={() => this.setState({ step: 1 })}
-                    buttonText={<FormattedMessage id="next"/>}
-                    usingUser={this.props.usingUser}
-                  />
-                {/* </Col>
+              <ChooseCards
+                choosable={this.state.step === 1}
+                header={intl.formatMessage({ id: 'chooseWantCard' })}
+                title={intl.formatMessage({ id: 'selectAll' })}
+                choosed={this.state.wantCards}
+                onToggle={this.handleWant}
+                onClickNext={() => this.setState({ step: 2 })}
+                onClickExpand={() => this.setState({ step: 1 })}
+                buttonText={intl.formatMessage({ id: 'next' })}
+                usingUser={this.props.usingUser}
+              />
+              {/* </Col>
               </Row> */}
               {/* <Row>
                 <Col> */}
-                {this.state.step >= 2 ?  
-                  <ChooseCards
-                    choosable={this.state.step === 2}
-                    header={<FormattedMessage id='offerCard'/>}
-                    title={<FormattedMessage id='selectAll'/>}
-                    choosed={this.state.offerCards}
-                    onToggle={this.handleOffer}
-                    onClickNext={() => {this.postTradeOffer();}/*() => this.setState({ step: 3 })*/}
-                    onClickExpand={() => this.setState({ step: 2 })}
-                    buttonText={<FormattedMessage id='Done'/>}
-                    checkSecond={true}
-                    usingUser={this.props.usingUser}
-                    textAreaTime={this.state.textAreaTime}
-                    textAreaClan={this.state.textAreaClan}
-                    textAreaPlayerName={this.state.textAreaPlayerName}
-                    textAreaPlayerTag={this.state.textAreaPlayerTag}
-                    handleOnChangeTime={this.handleOnChangeTime}
-                    handleOnChangeClan={this.handleOnChangeClan}
-                    handleOnChangePlayerName={this.handleOnChangePlayerName}
-                    handleOnChangePlayerTag={this.handleOnChangePlayerTag}
-                    emptyTimeError={this.state.emptyTimeError}
-                    emptyClanError={this.state.emptyClanError}
-                    emptyPlayerNameError={this.state.emptyPlayerNameError}
-                  />
-                  :
-                  <div></div>
-                }
-                {/* </Col>
+              {this.state.step >= 2 ? (
+                <ChooseCards
+                  choosable={this.state.step === 2}
+                  header={intl.formatMessage({ id: 'chooseOfferCard' })}
+                  title={intl.formatMessage({ id: 'selectAll' })}
+                  choosed={this.state.offerCards}
+                  onToggle={this.handleOffer}
+                  onClickNext={
+                    () => {
+                      this.postTradeOffer();
+                    } /*() => this.setState({ step: 3 })*/
+                  }
+                  onClickExpand={() => this.setState({ step: 2 })}
+                  buttonText={intl.formatMessage({ id: 'done' })}
+                  checkSecond={true}
+                  usingUser={this.props.usingUser}
+                  textAreaTime={this.state.textAreaTime}
+                  textAreaClan={this.state.textAreaClan}
+                  textAreaPlayerName={this.state.textAreaPlayerName}
+                  textAreaPlayerTag={this.state.textAreaPlayerTag}
+                  handleOnChangeTime={this.handleOnChangeTime}
+                  handleOnChangeClan={this.handleOnChangeClan}
+                  handleOnChangePlayerName={this.handleOnChangePlayerName}
+                  handleOnChangePlayerTag={this.handleOnChangePlayerTag}
+                  emptyTimeError={this.state.emptyTimeError}
+                  emptyClanError={this.state.emptyClanError}
+                  emptyPlayerNameError={this.state.emptyPlayerNameError}
+                />
+              ) : (
+                <div></div>
+              )}
+              {/* </Col>
               </Row> */}
             </Container>
           </Modal.Content>
@@ -177,5 +185,6 @@ export default class ChooseCardsModal extends React.Component{
       </div>
     );
   }
-};
+}
 
+export default injectIntl(ChooseCardsModal);
